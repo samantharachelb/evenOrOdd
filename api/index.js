37355c9950089b7b6afc1a23566cf82b69afab97
Samantha Emily-Rachel Belnavis <samanthabelnavis@gmail.com>
2020-03-06 00:49:22 -0500

    changed api route

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use(
     })
 );
 
-app.get('/api/*', (req, res) => {
+app.get('/', (req, res) => {
     const input = decodeURI(req.originalUrl.substr(5)).trim();
 
     if (!input.match(/^\d+$/)) {
